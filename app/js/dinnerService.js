76f6b0a4eeeb789706bea3623bd4ac4205a48302
zehuashan <zehua.shan@hotmail.com>
2016-03-04 20:48:56 +0100

    try2

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -5,28 +5,128 @@
 // the next time.
 dinnerPlannerApp.factory('Dinner',function ($resource) {
   
-  var numberOfGuest = 2;
-
-
-  this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
-  }
-
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
-
+  var numberOfGuest = 4;
 
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
   // you will need to modify the model (getDish and getAllDishes) 
   // a bit to take the advantage of Angular resource service
   // check lab 5 instructions for details
+  var savedDish = 1;
+    
+  this.trackId = function(id) {
+      savedDish = id;
+    }
+    
+  this.giveId = function() {
+      return savedDish;
+    }
+  
+  var menu = [];
 
 
 
+  this.setNumberOfGuests = function (num) {
+    //TODO Lab 2
+    if (num > 0) {
+    numberOfGuests = num;
+    }
+  }
+
+  // should return 
+  this.getNumberOfGuests = function () {
+    //TODO Lab 2
+    return numberOfGuests;
+  }
+
+  //Returns the dish that is on the menu for selected type 
+  this.getSelectedDish = function (type) {
+    //TODO Lab 2
+    for(key in menu) {
+      if(menu[key].Category == type) {
+        return menu[key];
+      }
+    }
+  }
+
+  //Returns all the dishes on the menu.
+  this.getFullMenu = function () {
+    //TODO Lab 2
+        return menu;
+  }
+
+  //Returns all ingredients for all the dishes on the menu.
+  this.getAllIngredients = function() {
+    //TODO Lab 2
+        var allIngredients = new Array();
+        for (key in menu) {
+            for(i in menu[key].Ingredients) {
+                allIngredients.push(menu[key].Ingredients[i]);
+            }
+        }
+        return allIngredients;
+  }
+
+  //Returns the total price of the menu (all the ingredients multiplied by number of guests).
+  this.getTotalMenuPrice = function() {
+    //TODO Lab 2
+        var totalPrice = 0;
+        allIngredients = this.getAllIngredients();
+        for (key in allIngredients) {
+            totalPrice += allIngredients[key].Quantity;
+        }
+        return totalPrice * numberOfGuests;
+  }
+
+  this.getDishPrice = function (inDish) {
+    var price = 0;
+    for (key in inDish.Ingredients) {
+      price += inDish.Ingredients[key].Quantity;
+    }
+    price = price * numberOfGuests;
+    return price;
+  }
+
+  //Adds the passed dish to the menu. If the dish of that type already exists on the menu
+  //it is removed from the menu and the new one added.
+  this.addDishToMenu = function(inDish) {
+    //TODO Lab 2
+        var dish = inDish;
+        for(key in menu) {
+          if(menu[key].Category == dish.Category) {
+            this.removeDishFromMenu(menu[key].RecipeID);
+          }
+        }
+        menu.push(dish);
+  }
+
+  //Removes dish from menu
+  this.removeDishFromMenu = function(id) {
+    //TODO Lab 2
+        for(key in menu) {
+          if(menu[key].RecipeID == id) {
+            delete menu[key];
+          }
+        }
+  }
+
+  this.getDishName = function (id) {
+    for (key in dishes) {
+      if(dishes[key].RecipeID == id) {
+        return dishes[key].Title;
+      }
+    }
+  }
+  
+  // ---------------------------------Lab 4 starts here!-------------------------------------
+
+  // API key for BigOven data.
+  var apiKey = "H9n1zb6es492fj87OxDtZM9s5sb29rW3";
+  this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:apiKey});
+  this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:apiKey}); 
 
 
+ 
   // Angular service needs to return an object that has all the
   // methods created in it. You can consider that this is instead
   // of calling var model = new DinnerModel() we did in the previous labs
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -4,5 +4,14 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
+   $scope.search = function(query) {
+   $scope.status = "Searching...";
+   Dinner.DishSearch.get({title_kw:query},function(data){
+     $scope.dishes=data.Results;
+     $scope.status = "Showing " + data.Results.length + " results";
+   },function(data){
+     $scope.status = "There was an error";
+   });
+ }
 
 });
